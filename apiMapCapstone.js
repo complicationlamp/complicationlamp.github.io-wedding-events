d9b115195d7588490fa778daf0622ce2cae34f82
Kyle Looker <lookerkj@protonmail.com>
2018-03-18 15:42:05 -0700

    map to silvercreek valley CC

--- apiMapCapstone.js
+++ apiMapCapstone.js
@@ -3,9 +3,9 @@
 // I think I can do a initMap for each day set up listeners on the days 
 // then have them call a function for each day
 function initMap() {
-	var uluru = {lat: -25.363, lng: 131.044};
+	var uluru = {lat: 37.276759, lng: -121.771754};
     var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
+      zoom: 9,
       center: uluru
     });
     var marker = new google.maps.Marker({

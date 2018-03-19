52485f0ac20a23b65bfc04079dc474991fd26384
Kyle Looker <lookerkj@protonmail.com>
2018-03-18 17:32:40 -0700

    info containter fills on click, added README

---
+++ README.txt
@@ -0,0 +1,1 @@
+This webpage details events of my wedding including their location in the google map api. click on a day to see the days events and watch the map populate with markers of the events locations.
--- apiMapCapstone.js
+++ apiMapCapstone.js
@@ -1,4 +1,18 @@
 
+//make an object of all my info
+const daysEventsObject = {
+	Thursday: {
+		day: 'Thursday',
+		event1:'Mehendi',
+		location1: 'this is at the Peddireddy home',
+		address:'this is a test of the address'
+	},
+	Friday: {	
+		day: 'Friday',
+		event1:'Mehendi',
+		location1: 'this is at the Peddireddy home',
+		address:'this is a test of the address'
+	}}
 
 // I think I can do a initMap for each day set up listeners on the days 
 // then have them call a function for each day
@@ -14,3 +28,26 @@ function initMap() {
     });
 }
 
+//this function will call the id of the day, on click
+//it will need to clear the container
+//and call function of constants for the day's events
+function listOfEventsByDay() {
+	console.log('first ran');
+	$('#thursdayButton').click(function() {
+		console.log('this ran');
+		// return `thngsssssss`;
+	
+		//remove the box contencts
+		$(".days-stuff").empty();
+		$(".days-stuff").append(daysEventsObject['Thursday'].day);
+	}) 
+}
+
+
+
+//Thursday function displays the contents of the day with 
+//this will link to a popup of a video of the events 
+
+//Friday, we will need to clear the map and repopulate with 
+//with info for friday 
+$(listOfEventsByDay);

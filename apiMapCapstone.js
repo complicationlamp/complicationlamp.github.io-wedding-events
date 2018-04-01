407faf47f11f84b8e3e59177ce08bbfbbb3d339c
k <lookerkj@protonmail.com>
2018-03-31 18:20:25 -0700

    merging code from dragable directions api

--- apiMapCapstone.js
+++ apiMapCapstone.js
@@ -63,6 +63,17 @@ function initMap(centerpoint) {
       zoom: zoomMove,
       center: {lat: parseFloat(center[0]), lng: parseFloat(center[1])}
     });
+    //this adds the draggable directions api
+    var directionsService = new google.maps.DirectionsService();
+    var directionsDisplay = new google.maps.DirectionsRenderer({
+        draggable: true,
+        map: map,
+        panel: document.getElementById('right-panel')
+    });
+    directionsDisplay.addListener('directions_changed', function() {
+        computeTotalDistance(directionsDisplay.getDirections());
+    });
+    displayRoute('San Fransico, CA', 'San Jose, CA', directionsService, directionsDisplay);   
 
 //this calls the info box
 	var infowindowMehendi = new google.maps.InfoWindow({
@@ -150,6 +161,37 @@ function initMap(centerpoint) {
     });
 
 
+}
+
+//to display the route
+function displayRoute(origin, destination, service, display) {
+  service.route({
+      origin: origin,
+      destination: destination,
+      waypoints: [{
+          location: 'San Mateo, CA'
+      }, {
+          location: 'Santa Cruz,CA'
+      }],
+      travelMode: 'DRIVING',
+      avoidTolls: true
+  }, function(response, status) {
+      if (status == 'OK') {
+          display.setDirections(response);
+      } else {
+          alert('Could not display directions due to: ' + status);
+      }
+  });
+}
+
+function computeTotalDistance(result) {
+  var total = 0;
+  var myroute = result.routes[0];
+  for (var i =0; i < myroute.legs.length; i++) {
+      total +=  myroute.legs[i].distance.value;
+  }
+  total = total / 1000;
+  document.getElementById('total').innerHTML = total + ' km';
 }
 
 function closeCurrentInfoWindow() {
